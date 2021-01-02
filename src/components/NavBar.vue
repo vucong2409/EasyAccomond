19033c2aafe2f21c74c7500d9c515d08b12b4ba1
Inco1122 <vucong2409@gmail.com>
2021-01-02 19:39:21 +0700

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <b-navbar type="dark">
+            <b-navbar-brand href="#" class="float-left">EasyAccomond</b-navbar-brand>
+
+            <b-navbar-nav class="mr-5">
+                <b-nav-item href="#">Home</b-nav-item>
+                <b-nav-item href="#">About</b-nav-item>
+            </b-navbar-nav>
+
+            <b-navbar-nav class="ml-auto">
+                <b-nav-item href="#">Sign in</b-nav-item>
+                <b-nav-item href="#">Sign up</b-nav-item>
+                <b-nav-item href="#">Get started</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'NavBar',
+    props : {},
+    components : {
+    },
+    methods : {
+
+    }
+}
+</script>
+
+<style scoped>
+    .nav-masterhead .nav-link {
+        padding: .25rem 0;
+    }
+    
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cover-container d-flex h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto w-100"> 
+      <NavBar /> 
+    </header>
+
+    <main class="px-3">
+      <h1><b>Easy Accomond</b></h1>
+      <p class="text-gray"> The<b class="text-white"> #1 </b>house rental app in Vietnam </p>
+    </main>
+
+    <footer class="mt-auto">
+      <p> Created by <a href="https://twitter.com/vucong24092001" class="text-white"> @vucong2409 </a> </p>
+    </footer>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import NavBar from "../components/NavBar"
+export default {
+  name: 'Home',
+  components: {
+    NavBar
+  }
+}
+</script>
+
+<style scoped>
+  footer {
+    color : gray
+  }
+  
+  .cover-container {
+    max-width: 42em;
+  }
+
+  main {
+    font-size: 40px;
+  }
+
+  .text-gray {
+    color: gray;
+  }
+
+</style>
