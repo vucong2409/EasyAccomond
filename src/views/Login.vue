6bd37f7d56fb3cd5dcd2b48fd0a942ab1af9ab9e
Inco1122 <vucong2409@gmail.com>
2021-01-09 10:36:53 +0700

    feat: add Login pane

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,13 +4,25 @@
         </header>
 
         <main class="px-auto">
-            <b-tabs class="bg-white shadow w-50 mx-auto" justified >
-                <b-tab title="Login">
-                    <div class="">
+            <b-tabs class="bg-white shadow w-25 mx-auto text-dark" justified >
+                <b-tab title="Login" class="p-5">
+                    <div class="w-100 h-100">
+                        <router-link to="/" class="text-dark"><h1 class="mb-4">EzAccmd</h1></router-link>
+                        <b-input-group class="mx-auto w-75 my-3 shadow" >
+                           <b-form-input  class="login-input" :state="usernameState" placeholder="Username" v-model="username" aria-describedby="username-input"></b-form-input>
+                        </b-input-group>
+                        <b-form-invalid-feedback id="username-input">Enter at least 4 character </b-form-invalid-feedback>
+
+                        <b-input-group class="mx-auto w-75 my-3 shadow">
+                            <b-form-input type="password" class="login-input" placeholder="Password"></b-form-input>
+                        </b-input-group>
+
+                        <b-button squared class="shadow " variant="success" size="lg">Login</b-button>
+                        <br>
                     </div>
                 </b-tab>
 
-                <b-tab title="Sign up">
+                <b-tab title="Sign up" class="p-5">
                     <div class="">
                     </div>
                 </b-tab>
@@ -30,10 +42,25 @@
         padding: 1em 1em 1em 1em;
     }
 
+    .title-pane {
+        margin-bottom: 3rem ;
+    }
+    
+
 </style>
 
 <script>
 export default {
-    name : 'Login'
+    name : 'Login',
+    computed: {
+        usernameState() {
+            return this.username.length > 5 ? true : false
+        }
+    },
+    data() {
+        return {
+            username : ""
+        } 
+    }
 }
 </script>
